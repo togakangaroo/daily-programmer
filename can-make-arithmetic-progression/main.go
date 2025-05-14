28dda1bf4cdf145b906e4793add6fc73eb9c1a2d
George Mauer <gmauer@gmail.com>
2025-05-14 07:57:29 -0500

    simple implementation

---
+++ can-make-arithmetic-progression/main.go
@@ -0,0 +1,47 @@
+// Simple Implementation
+// :PROPERTIES:
+// :header-args+: :noweb strip-export
+// :header-args:go+: :mkdirp 't :tangle main.go :comments both
+// :END:
+
+// Ok lets do it based on the deeper insight above. It becomes quite simple
+
+
+// [[file:README.org::*Simple Implementation][Simple Implementation:1]]
+package main
+
+
+
+import (
+  "fmt"
+  "slices"
+)
+
+func isAlgorithmicProgression(array []int) bool {
+  if len(array) <= 2 {
+    return true
+  }
+
+  sortedArray := slices.Clone(array)
+  slices.Sort(sortedArray)
+
+  prevItem := sortedArray[1]
+  prevGap := prevItem - sortedArray[0]
+
+  for _, item := range sortedArray[2:] {
+    newGap := item - prevItem
+    if newGap != prevGap {
+      return false
+    }
+    prevGap = newGap
+    prevItem = item
+  }
+
+  return true
+}
+
+func main() {
+  fmt.Println(isAlgorithmicProgression([]int{3, 5, 1}))
+  fmt.Println(isAlgorithmicProgression([]int{1, 2, 4}))
+}
+// Simple Implementation:1 ends here
